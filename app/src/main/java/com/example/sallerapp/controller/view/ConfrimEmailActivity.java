87dd94f2c7aac4a630a095b29742bad229517769
vanh <vanhcoder8386@gmail.com>
2023-10-31 14:17:46 +0700

    layout dialog

---
+++ app/src/main/java/com/example/sallerapp/controller/view/ConfrimEmailActivity.java
@@ -0,0 +1,16 @@
+package com.example.sallerapp.controller.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sallerapp.R;
+
+public class ConfrimEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confrim_email);
+    }
+}
