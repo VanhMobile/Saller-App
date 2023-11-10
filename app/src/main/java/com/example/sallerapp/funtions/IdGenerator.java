2c31c1a12c764c308d6ffc647c55b967c41018a9
vanh <vanhcoder8386@gmail.com>
2023-11-10 10:03:12 +0700

    update validations

--- app/src/main/java/com/example/sallerapp/funtions/IdGenerator.java
+++ app/src/main/java/com/example/sallerapp/funtions/IdGenerator.java
@@ -6,7 +6,7 @@ import android.preference.PreferenceManager;
 
 public class IdGenerator {
 
-    // hàm tạo id shop tự động dùng biến count cho id tặng tự động , dùng sharedperferences để lưu trữ.
+    // hàm tạo id shop tự động dùng biến count cho id tặng tự động
 
     public static String generateNextShopId(int count, String s) {
         String Id = s + ++count;
