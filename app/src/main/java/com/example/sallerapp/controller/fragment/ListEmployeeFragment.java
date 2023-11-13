e45d58ff424eb7025102260102f17123f6b0989e
tudev <tukieu954@gmail.com>
2023-11-13 13:44:56 +0700

    emloyee

--- app/src/main/java/com/example/sallerapp/controller/fragment/ListEmployeeFragment.java
+++ app/src/main/java/com/example/sallerapp/controller/fragment/ListEmployeeFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -53,6 +55,23 @@ public class ListEmployeeFragment extends Fragment {
     private void initView() {
         AdRequest adRequest = new AdRequest.Builder().build();
         employBinding.adView.loadAd(adRequest);
+
+        employBinding.searchEm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adapter.seachEmployee(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         EmployeeDao.getEmployees("Shop_1", new EmployeeDao.GetData() {
             @Override
