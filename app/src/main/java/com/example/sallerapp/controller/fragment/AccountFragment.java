fd21d64e9d2daf6f177c6fc8ecc05f37693c0600
vanh <vanhcoder8386@gmail.com>
2023-11-20 00:10:26 +0700

    home logic and bar chart logic

--- app/src/main/java/com/example/sallerapp/controller/fragment/AccountFragment.java
+++ app/src/main/java/com/example/sallerapp/controller/fragment/AccountFragment.java
@@ -1,6 +1,8 @@
 package com.example.sallerapp.controller.fragment;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -17,17 +19,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.sallerapp.controller.view.LoginActivity;
 import com.example.sallerapp.database.AccountDao;
 import com.example.sallerapp.databinding.FragmentAccountBinding;
 import com.example.sallerapp.desgin_pattern.build_pantter.AccountBuilder;
 import com.example.sallerapp.desgin_pattern.single_pantter.SingleAccount;
 import com.example.sallerapp.funtions.Validations;
 import com.example.sallerapp.model.ShopAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class AccountFragment extends Fragment {
 
     private FragmentAccountBinding accountBinding;
+    ShopAccount shopAccount = SingleAccount.getInstance().getShopAccount();
+    private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient googleSignInClient;
+
 
     public AccountFragment() {
         // Required empty public constructor
@@ -46,12 +59,50 @@ public class AccountFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         accountBinding = FragmentAccountBinding.inflate(inflater, container, false);
-
         initView();
         return accountBinding.getRoot();
     }
 
     private void initView() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso);
+        accountBinding.afNameShop.setText(shopAccount.getShopName());
+        accountBinding.afAddressShop.setText(shopAccount.getAddress());
+        accountBinding.afEmailShop.setText(shopAccount.getEmail());
+        accountBinding.afPhonenumberShop.setText(shopAccount.getNumberPhone());
 
+        accountBinding.btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { 
+                logout();
+            }
+        });
+    }
+
+    private void logout() {
+        AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
+        builder.setTitle("Thông báo");
+        builder.setMessage("Bạn có chắc chắn muốn đăng xuất không ?");
+        builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                firebaseAuth.signOut();
+                googleSignInClient.signOut();
+                Intent intent = new Intent(requireContext(), LoginActivity.class);
+                startActivity(intent);
+                requireActivity().finish();
+                // Chuyển đến màn hình đăng nhập hoặc màn hình khác theo nhu cầu của bạn
+            }
+        });
+        builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
     }
 }
